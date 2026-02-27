d213484be6edafdd4e9564d04fb713343acdb44e
Your Name <mdhosen21018@gmail.com>
2026-02-27 15:12:08 +0600

    dashboard update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,11 @@ import { getAllBlogs } from "@/services/Blog";
 
 const DashboardPage = async () => {
   const blogPosts = await getAllBlogs();
-  return <Dashboard blog={blogPosts?.data} />;
+  return (
+    <>
+      <Dashboard blog={blogPosts?.data} />
+    </>
+  );
 };
 
 export default DashboardPage;
