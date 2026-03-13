91d475a6f9da86e0d811b1c4d9a6fa9064be8d15
Your Name <mdhosen21018@gmail.com>
2026-03-13 19:10:04 +0600

    mail

--- src/app/dashboard/mail/page.tsx
+++ src/app/dashboard/mail/page.tsx
@@ -9,11 +9,12 @@ type Mail = {
 };
 const mailPage = async () => {
   const getmails = await getAllMails();
+  console.log(getmails);
   return (
     <div className="container mx-auto p-4">
       All mails
       {getmails?.data?.map((mail: Mail, index: number) => {
-        <div className="" key={index}>
+        <div className="border shadow-md" key={index}>
           <p>Subject: {mail?.subject}</p>
           <p>Mail: {mail?.mailId}</p>
           <p>Body: {mail?.body}</p>
