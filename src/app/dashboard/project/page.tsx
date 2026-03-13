248887e895dcc94f29a6480ab1c0857866ac31b5
Your Name <mdhosen21018@gmail.com>
2026-03-13 15:59:27 +0600

    Project

--- src/app/dashboard/project/page.tsx
+++ src/app/dashboard/project/page.tsx
@@ -1,12 +1,13 @@
-"use client"
+// "use client"
 import NewProjectForm from "@/components/dashboard/NewProjectForm";
+import { getAllCategory } from "@/services/Category";
 
-const projectPage = () => {
+const projectPage = async () => {
+  const categories = await getAllCategory();
   return (
     <div className="container mx-auto px-4">
       Project
-
-      <NewProjectForm />
+      <NewProjectForm categories={categories?.data} />
     </div>
   );
 };
