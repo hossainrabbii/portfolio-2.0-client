fc77cc8f83f8ae6b81e21053c6cf8d5fb16115e9
Your Name <mdhosen21018@gmail.com>
2026-03-13 15:54:24 +0600

    Project

--- src/app/dashboard/project/page.tsx
+++ src/app/dashboard/project/page.tsx
@@ -1,10 +1,12 @@
+"use client"
 import NewProjectForm from "@/components/dashboard/NewProjectForm";
 
 const projectPage = () => {
   return (
     <div className="container mx-auto px-4">
-      {/* <NewProjectForm /> */}
       Project
+
+      <NewProjectForm />
     </div>
   );
 };
